ffc2e5d40e6462e629d631ecd64561f809d49aea
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-06-20 15:14:48 +0200

    add table structure named tabledata

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo }from 'react'
+import axios from 'axios';
+import './App'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ // data state to store the Users API data. 
+ //Initially it  is an empty array.
+ const [data,setData] = useState([]);
+ 
+ useEffect(() => {
+  (async () => {
 
-export default App;
+    const result = await axios("https://jsonplaceholder.typicode.com/users");
+    setData(result.data);
+    console.log(result.data)
+  })();
+ }, []);
+
+ const tabledata = useMemo(
+   () => [
+    {
+      //first group - Users
+      Header: 'Users',
+        // group columns
+      columns: [
+        {
+          Header: "Id",
+          accessor: "name"
+        },
+        {
+          Header: "Name",
+          accessor: "name"
+        },
+        {
+          Header: "User Name",
+          accessor: "username"
+        }
+      ]
+    },
+    {//  Second group - Contact Details
+      Header: "Contact Details",
+      // group columns
+      columns: [
+        {
+          Header: "Phone",
+          accessor: "phone"
+        },
+        {
+          Header: "Email",
+          accessor: "email"
+        },
+        {
+          Header: "Website",
+          accessor: "website"
+        }
+      ]
+    }
+  ],
+  []
+ );
+
+ return (
+   <div className="App">
+     <Table columns={tabledata} data={data} />
+   </div>
+ )
+
+}
