fcb1130b154fb333bb018fc5e53389dfb8a802cd
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-06-20 16:04:24 +0200

    add filtering option for name attribute

--- src/table.js
+++ src/table.js
@@ -1,19 +1,35 @@
 import React, { useState, } from 'react'
 import { useTable } from "react-table"
 
+// Create a state
+const [filterInput, stFilterInput] = useState("");
 // Use state and functions treturnd from usetable to build UI
 const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow
+    prepareRow, 
+    setFilter
 } = useTable(
     {
         columns,
         data
-    }
+    },
+    useFilters // Adding the useFilters Hook to the table 
 );
+
+const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("name", value);
+};
+
+// Input element
+<input
+    value={filterInput}
+    onChange={handleFilterChange}
+    placeholder={"Search name"}
+/>
 
 // Render the UI for table
 return (
